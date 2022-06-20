c4ee7c2b7e0cff3f073fa74fe24f05ee86b1a2cf
jacopofabi <jacopo.fabi1997@gmail.com>
2022-06-20 10:25:14 +0200

    producer from csv

--- src/main/java/flink/KafkaConnectorDemo.java
+++ src/main/java/flink/KafkaConnectorDemo.java
@@ -4,12 +4,14 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import utils.ValQ1;
 
 import java.time.Duration;
 
@@ -33,60 +35,11 @@ public class KafkaConnectorDemo {
         var src = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
         var dataStream = src
-                .map(new MapFunction<String, Message>() {
-                    @Override
-                    public Message map(String value) throws Exception {
-                        return Message.create(value);
-                    }
-                })
-                .map((MapFunction<Message, Tuple3<Integer, Long, Float>>) value -> Tuple3.of(1, value.getTimestamp(), value.getRandom()))
-                    .returns(Types.TUPLE(Types.INT, Types.LONG, Types.FLOAT))
-                .assignTimestampsAndWatermarks(
-                        WatermarkStrategy
-                        .<Tuple3<Integer, Long, Float>>forMonotonousTimestamps()
-                            // forBoundedOutOfOrderness(Duration.ofSeconds(1))
-                        .withTimestampAssigner((tuple, timestamp) -> tuple.f1)
-                        .withIdleness(Duration.ofSeconds(10))
-                )
-                .windowAll(TumblingEventTimeWindows.of(Time.seconds(10)))
-                .sum(0);
-        dataStream.print();
+                .map(value -> Tuple2.of(ValQ1.create(value), 1))
+                .returns(Types.TUPLE(Types.GENERIC(ValQ1.class), Types.INT))
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(10)));
+        //dataStream.print();
 
         env.execute("Kafka Connector Demo");
-    }
-
-
-    public static class Message{
-        public int sequence;
-        public long timestamp;
-        public String message;
-        public float random;
-
-        public static Message create(String rawMessage){
-            var values = rawMessage.split(";");
-            return new Message(Integer.parseInt(values[0]),
-                    Long.parseLong(values[1]), values[2],
-                    Float.parseFloat(values[3].replace(",",".")));
-        }
-
-        private Message(int sequence, long timestamp, String message, float random){
-            this.sequence = sequence;
-            this.timestamp = timestamp;
-            this.message = message;
-            this.random = random;
-        }
-
-        public float getRandom() {
-            return random;
-        }
-        public int getSequence() {
-            return sequence;
-        }
-        public long getTimestamp() {
-            return timestamp;
-        }
-        public String getMessage() {
-            return message;
-        }
     }
 }
--- src/main/java/kafka/Producer.java
+++ src/main/java/kafka/Producer.java
@@ -3,11 +3,19 @@ package kafka;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.Random;
 
 public class Producer {
-    public static void main(String[] args) throws InterruptedException {
+    private static final String COMMA_DELIMITER = ";";
+
+    public static void main(String[] args) throws InterruptedException, IOException, ParseException {
 
         //properties for producer
         Properties props = new Properties();
@@ -16,24 +24,39 @@ public class Producer {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         //create producer
-        org.apache.kafka.clients.producer.Producer <Integer, String> producer = new KafkaProducer<Integer, String>(props);
+        org.apache.kafka.clients.producer.Producer<Object, String> producer = new KafkaProducer<>(props);
 
-        long ts = System.currentTimeMillis();
-        var rnd = new Random();
-
-        //send messages to my-topic
-        int j = 1;
-        while (true){
-            for(int i = 0; i < 100; i++) {
-                ts = System.currentTimeMillis();
-                var message = String.format("%d;%d;%s;%f", 100 * (j-1) + i, ts, "Test Message #" + i, rnd.nextDouble());
-                var producerRecord = new ProducerRecord<>("flink-events", i, message);
-                producer.send(producerRecord);
-                System.out.printf("Send: %s%n", message);
+        boolean first = true;
+        Timestamp previous = null;
+        while (true) {
+            BufferedReader br = new BufferedReader(new FileReader("data/2022-05_bmp180.csv"));
+            String line = br.readLine(); //skip the header
+            System.out.println("Header: " + line);
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(COMMA_DELIMITER);
+                if (values.length == 10 && !values[5].isEmpty()) {
+                    var date = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss").parse(values[5]);
+                    Timestamp timestamp = new Timestamp(date.getTime());
+                    Long sensor_id = Long.parseLong(values[0]);
+                    Double temperature = Double.parseDouble(values[9]);
+                    var message = String.format("%s;%d;%,.4f;", timestamp, sensor_id, temperature);
+                    var producerRecord = new ProducerRecord<>("flink-events", message);
+                    if (first) {
+                        first = false;
+                    }
+                    else {
+                        long diff = (timestamp.getTime() - previous.getTime());
+                        Thread.sleep(diff);
+                    }
+                    producer.send(producerRecord);
+                    System.out.printf("Send: %s%n", message);
+                    previous = timestamp;
+                }
+                else {
+                    //do nothing, corrupted record
+                }
             }
-            producer.flush();
-            j = j+1;
-            Thread.sleep(5000);
+            br.close();
         }
     }
 }
