899d85df9a96e9be514520bba5f72a8c781ecd7b
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-06-20 15:44:06 +0200

    Auto stash before merge of "danilo" and "mich"

--- src/main/java/flink/KafkaConnectorDemo.java
+++ src/main/java/flink/KafkaConnectorDemo.java
@@ -6,6 +6,7 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
@@ -34,20 +35,20 @@ public class KafkaConnectorDemo {
 
         DataStreamSource<String> src = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        SingleOutputStreamOperator<Tuple2<Long, ValQ1>> test = src.map(new MapFunction<String, Tuple2<Long, ValQ1>>() {
-                    @Override
-                    public Tuple2<Long, ValQ1> map(String s) throws Exception {
-                        ValQ1 val = ValQ1.create(s);
-                        return Tuple2.of(val.getSensor_id(), val);
-                    }
-                })
-                .assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple2<Long, ValQ1>>forMonotonousTimestamps()
-                        .withTimestampAssigner((tuple, l) -> tuple.f1.getTimestamp().getTime())
-                        .withIdleness(Duration.ofSeconds(20))
-                )
-                .keyBy(value -> value.f0)
-                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
-                .sum(0);
+        /*
+        var dataStream = src
+                .map(value -> Tuple2.of(ValQ1.create(value), 1))
+                .returns(Types.TUPLE(Types.GENERIC(ValQ1.class), Types.INT))
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(10)));
+        */
+        SingleOutputStreamOperator<Tuple2<Timestamp, ValQ1>> test = src.map(new MapFunction<String, Tuple2<Timestamp, ValQ1>>() {
+
+            @Override
+            public Tuple2<Timestamp, ValQ1> map(String s) throws Exception {
+                ValQ1 val = ValQ1.create(s);
+                return Tuple2.of(val.getTimestamp(), val);
+            }
+        });
         test.print();
 
         env.execute("Kafka Connector Demo");
