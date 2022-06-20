f424497a095045ac5c43f1870ca3253c251ef7a8
mich <michelesalvv@gmail.com>
2022-06-20 17:59:02 +0200

    test

---
+++ src/main/java/flink/AccumulatorQuery1.java
@@ -0,0 +1,8 @@
+package flink;
+
+import utils.ValQ1;
+
+public class AccumulatorQuery1 {
+    long count;
+    Double sum;
+}
--- src/main/java/flink/KafkaConnectorDemo.java
+++ src/main/java/flink/KafkaConnectorDemo.java
@@ -1,29 +1,30 @@
 package flink;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import scala.Enumeration;
 import utils.ValQ1;
 
 import java.time.Duration;
 
 public class KafkaConnectorDemo {
-
+    //TODO Fare un serializzatore vero
     /*
      * Before running this program, load data in Kafka using kafka-producer.
      */
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
         KafkaSource<String> source = KafkaSource.<String>builder()
                 .setBootstrapServers("kafka-broker:9092")
                 .setTopics("flink-events")
@@ -31,23 +32,19 @@ public class KafkaConnectorDemo {
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
-
         var src = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        SingleOutputStreamOperator<Tuple2<Long, ValQ1>> test = src.map(new MapFunction<String, Tuple2<Long, ValQ1>>() {
-                    @Override
-                    public Tuple2<Long, ValQ1> map(String s) throws Exception {
-                        ValQ1 val = ValQ1.create(s);
-                        return Tuple2.of(val.getSensor_id(), val);
-                    }
-                })
-                .assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple2<Long, ValQ1>>forMonotonousTimestamps()
-                        .withTimestampAssigner((tuple, l) -> tuple.f1.getTimestamp().getTime())
-                        .withIdleness(Duration.ofSeconds(20))
+        SingleOutputStreamOperator<Double> test = src.map((MapFunction<String, ValQ1>) s -> {
+            return ValQ1.create(s);
+        })
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<ValQ1>forMonotonousTimestamps()
+                        .withTimestampAssigner((val, l) -> val.getTimestamp().getTime())
+//                        .withIdleness(Duration.ofMillis(1))
                 )
-                .keyBy(value -> value.f0)
+                .keyBy(val -> val.getSensor_id())
                 .window(TumblingEventTimeWindows.of(Time.seconds(10)))
-                .sum(0);
+                .aggregate(new AggregatorQuery1());
+
         test.print();
 
         env.execute("Kafka Connector Demo");
