ac5d4cd9b7cf425d5482dc1867dea0676aabecac
mich <michelesalvv@gmail.com>
2022-06-21 10:07:25 +0200

    reduced pom, added scripts

---
+++ scripts/setup_env.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+## cancel previous flink jobs
+
+for ID in $(sudo docker exec -it jobmanager bash -c "flink list | grep RUNNING | awk '{print \$4}'")
+do 
+    sudo docker exec -it jobmanager bash -c "flink cancel $ID"
+done
+
+## clean kafka topic
+sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic flink-events
+
+## copy jar in flink's jobmanager
+sudo docker cp target/SABD_proj2-1.0-SNAPSHOT.jar jobmanager:/home
