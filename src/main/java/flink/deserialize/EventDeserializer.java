5572647ff9aaff7477d105791a98876f4d3c00a7
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-06-25 17:47:17 +0200

    Completed Exceptions

--- src/main/java/flink/deserialize/EventDeserializer.java
+++ src/main/java/flink/deserialize/EventDeserializer.java
@@ -1,8 +1,8 @@
 package flink.deserialize;
 
+import flink.exception.CoordinatesOutOfBoundException;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import flink.exception.CoordinatesOutOfBoundException;
 import flink.exception.TemperatureOutOfBoundException;
 
 import java.nio.charset.StandardCharsets;
@@ -14,8 +14,9 @@ public class EventDeserializer implements DeserializationSchema<Event> {
         Event event = new Event(new String(bytes, StandardCharsets.UTF_8));
         try{
             validateTemperature(event.getTemperature());
+            validateCoordinates(event.getLatitude(),event.getLongitude());
             return event;
-        } catch (TemperatureOutOfBoundException e) {
+        } catch (TemperatureOutOfBoundException | CoordinatesOutOfBoundException e) {
 //            e.printStackTrace();
             return null;
         }
@@ -39,6 +40,13 @@ public class EventDeserializer implements DeserializationSchema<Event> {
     static void validateTemperature(Double temperature) throws TemperatureOutOfBoundException {
         if (temperature < -40 || temperature > 85) {
             throw new TemperatureOutOfBoundException("Deserializer Error: Temperature out of Sensor Range");
+        }
+    }
+
+    // La temperatura massima rilevabile del sensore varia da -40° a +85°
+    static void validateCoordinates(Double latitude, Double longitude) throws CoordinatesOutOfBoundException {
+        if (latitude < -90D || latitude > 90D || longitude < -180D || longitude > 180D) {
+            throw new CoordinatesOutOfBoundException("Deserializer Error: Coordinates out of Bound");
         }
     }
 }
