3a4eb88fb27be1ad6efbf031352883a320982bd6
mich <michelesalvv@gmail.com>
2022-06-27 16:52:40 +0200

    script updated

--- scripts/cleanKafka.sh
+++ scripts/cleanKafka.sh
@@ -2,3 +2,4 @@
 
 sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic flink-events;
 sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics flink-events --force
+sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics output --force
