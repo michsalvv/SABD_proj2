d82a25e07da980b9519de5b9de5132518d750701
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-06-28 11:29:49 +0200

    Best Producer

--- src/main/java/kafka/Producer.java
+++ src/main/java/kafka/Producer.java
@@ -1,9 +1,12 @@
 package kafka;
 
+import org.apache.flink.shaded.netty4.io.netty.handler.codec.sctp.SctpOutboundByteStreamHandler;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import kafka.exception.SimulationTimeException;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import utils.Config;
 
 import java.io.BufferedReader;
@@ -12,8 +15,10 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.TimeZone;
 
 public class Producer {
 
@@ -35,8 +40,20 @@ public class Producer {
         while ((line = br.readLine()) != null) {
             String[] values = line.split(Config.COMMA_DELIMITER);
             if (values.length == 10 && !values[5].isEmpty()) {
+                Calendar cal = Calendar.getInstance();
                 var date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(values[5]);
-                Timestamp timestamp = new Timestamp(date.getTime());
+
+                DateTimeZone timeZone = DateTimeZone.forID( "Europe/Rome" );
+                DateTime dateTime = new DateTime( date, timeZone );
+                long ts = dateTime.getMillis();
+                Timestamp timestamp = new Timestamp(ts);
+//                System.out.println("-------------------------------------");
+//                System.out.println( "date: " + date );
+//                System.out.println( "dateTime: " + dateTime );
+//                System.out.println( "Millis: " + ts );
+//                System.out.println( "Our TS: " + timestamp );
+//                System.out.println("-------------------------------------");
+
                 Long sensor_id = Long.parseLong(values[0]);
                 Double temperature = Double.parseDouble(values[9]);
                 Long location = Long.parseLong(values[2]);
@@ -44,7 +61,8 @@ public class Producer {
                 Double longitude = Double.parseDouble(values[4]);
                 var message = String.format("%s;%d;%,.4f;%d;%,.4f;%,.4f;", timestamp, sensor_id, temperature,
                         location, latitude, longitude);
-                var producerRecord = new ProducerRecord<>("flink-events", message);
+                var kafka_ts = ts + Config.CEST;
+                var producerRecord = new ProducerRecord<>("flink-events", 0, kafka_ts, null,  message);
                 if (first) {
                     first = false;
                 } else {
