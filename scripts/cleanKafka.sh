ba03282645190c98ef8428ba3b7d0171723946d9
mich <michelesalvv@gmail.com>
2022-07-01 17:06:02 +0200

    fix KafkaQ2

--- scripts/cleanKafka.sh
+++ scripts/cleanKafka.sh
@@ -4,10 +4,17 @@ sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092
 sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q1-hourly;
 sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q1-weekly;
 sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q1-monthly;
+sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q2-hourly;
+sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q2-weekly;
+sudo docker exec kafka-broker kafka-topics --bootstrap-server kafka-broker:29092 --delete --topic q2-daily;
 
-sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics flink-events --force;
 
 
 sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q1-hourly --force;
 sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q1-weekly --force;
 sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q1-monthly --force;
+
+
+sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q2-hourly --force;
+sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q2-weekly --force;
+sudo docker exec kafka-broker kafka-streams-application-reset --bootstrap-servers kafka-broker:9092 --application-id  kafka-streams --input-topics q2-daily --force;
--- src/main/java/kafka/queries/Query2.java
+++ src/main/java/kafka/queries/Query2.java
@@ -19,14 +19,17 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Properties;
 
+import static org.apache.kafka.streams.kstream.Suppressed.BufferConfig.maxRecords;
+import static org.apache.kafka.streams.kstream.Suppressed.BufferConfig.unbounded;
+
 /**
  * Find the real-time top-5 ranking of locations
  * (location) having the highest average temperature
  * and the top-5 ranking of locations (location) having
  * the lowest average temperature
  * Output:  ts, location1, avg_temp1, ... location5,
- *          avg_temp5, location6, avg_temp6, ...
- *          location10, avg_temp10
+ * avg_temp5, location6, avg_temp6, ...
+ * location10, avg_temp10
  */
 public class Query2 extends Query {
     KStream<Integer, Event> src;
@@ -46,71 +49,94 @@ public class Query2 extends Query {
                 .filter((integer, event) -> event != null)
                 .selectKey((integer, event) -> event.getLocation());
 
-
-        var grouped = keyed
+        var hourlyStatistics = keyed
                 .groupByKey(Grouped.with(Serdes.Long(), CustomSerdes.Event()))
-                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofHours(1)));
-//                .windowedBy(new WeeklyWindow());
-
-        var statistics = grouped
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofHours(1)))
                 .aggregate(ValQ2::new, (aLong, event, valQ2) -> {
-                    Double temp = valQ2.getTemperature()+event.getTemperature();
+                    Double temp = valQ2.getTemperature() + event.getTemperature();
                     valQ2.setTemperature(temp);
-                    valQ2.setOccurrences(valQ2.getOccurrences()+1L);
-                    valQ2.setTimestamp(Tools.getHourSlot(event.getTimestamp()));            // Modificare qui in base alla finestra
+                    valQ2.setOccurrences(valQ2.getOccurrences() + 1L);
+                    valQ2.setTimestamp(Tools.getHourSlot(event.getTimestamp()));
                     valQ2.setLocation(event.getLocation());
                     valQ2.calculateMean();
                     return valQ2;
                 }, Materialized.with(Serdes.Long(), CustomSerdes.ValQ2()));
-        
-       var result = statistics
-               .groupBy((longWindowed, valQ2) -> new KeyValue<>(Tools.getHourSlot(valQ2.getTimestamp()).toString(), valQ2),     // Modificare qui in base alla finestra
-                       Grouped.with(Serdes.String(), CustomSerdes.ValQ2()))
 
-               .aggregate(LocationAggregator::new, (timestamp, valQ2, aggregator) -> {
-                   aggregator.setTimestamp(timestamp);
-                    aggregator.updateRank(valQ2);
-                    return aggregator;
-               }, (timestamp, valQ2, valQ2s) -> null,
-                       Materialized.with(Serdes.String(), CustomSerdes.LocationAggregator()));
+        var dailyStatistics = keyed
+                .groupByKey(Grouped.with(Serdes.Long(), CustomSerdes.Event()))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofDays(1)))
+                .aggregate(ValQ2::new, (aLong, event, valQ2) -> {
+                    Double temp = valQ2.getTemperature() + event.getTemperature();
+                    valQ2.setTemperature(temp);
+                    valQ2.setOccurrences(valQ2.getOccurrences() + 1L);
+                    valQ2.setTimestamp(Tools.getDaySlot(event.getTimestamp()));
+                    valQ2.setLocation(event.getLocation());
+                    valQ2.calculateMean();
+                    return valQ2;
+                }, Materialized.with(Serdes.Long(), CustomSerdes.ValQ2()));
 
-        result.toStream().print(Printed.toSysOut());
+        var weeklyStatistics = keyed
+                .groupByKey(Grouped.with(Serdes.Long(), CustomSerdes.Event()))
+                .windowedBy(new WeeklyWindow())
+                .aggregate(ValQ2::new, (aLong, event, valQ2) -> {
+                    Double temp = valQ2.getTemperature() + event.getTemperature();
+                    valQ2.setTemperature(temp);
+                    valQ2.setOccurrences(valQ2.getOccurrences() + 1L);
+                    valQ2.setTimestamp(Tools.getWeekSlot(event.getTimestamp()));
+                    valQ2.setLocation(event.getLocation());
+                    valQ2.calculateMean();
+                    return valQ2;
+                }, Materialized.with(Serdes.Long(), CustomSerdes.ValQ2()));
 
-        result.toStream().to("q2-weekly", Produced.with(Serdes.String(), CustomSerdes.Q2Output()));
+
+        var hourlyResults = hourlyStatistics
+                .groupBy((longWindowed, valQ2) -> new KeyValue<>(valQ2.getTimestamp().toString(), valQ2),
+                        Grouped.with(Serdes.String(), CustomSerdes.ValQ2()))
+
+                .aggregate(LocationAggregator::new, (timestamp, valQ2, aggregator) -> {
+                            aggregator.setTimestamp(timestamp);
+                            aggregator.updateRank(valQ2);
+                            return aggregator;
+                        }, (timestamp, valQ2, valQ2s) -> null,
+                        Materialized.with(Serdes.String(), CustomSerdes.LocationAggregator()));
+
+        var dailyResults = dailyStatistics
+                .groupBy((longWindowed, valQ2) -> new KeyValue<>(valQ2.getTimestamp().toString(), valQ2),
+                        Grouped.with(Serdes.String(), CustomSerdes.ValQ2()))
+
+                .aggregate(LocationAggregator::new, (timestamp, valQ2, aggregator) -> {
+                            aggregator.setTimestamp(timestamp);
+                            aggregator.updateRank(valQ2);
+                            return aggregator;
+                        }, (timestamp, valQ2, valQ2s) -> null,
+                        Materialized.with(Serdes.String(), CustomSerdes.LocationAggregator()));
+
+        var weeklyResults = weeklyStatistics
+                .groupBy((longWindowed, valQ2) -> new KeyValue<>(valQ2.getTimestamp().toString(), valQ2),
+                        Grouped.with(Serdes.String(), CustomSerdes.ValQ2()))
+
+                .aggregate(LocationAggregator::new, (timestamp, valQ2, aggregator) -> {
+                            aggregator.setTimestamp(timestamp);
+                            aggregator.updateRank(valQ2);
+                            return aggregator;
+                        }, (timestamp, valQ2, valQ2s) -> null,
+                        Materialized.with(Serdes.String(), CustomSerdes.LocationAggregator()));
+
+
+
+
+
+        hourlyResults.toStream().to("q2-hourly", Produced.with(Serdes.String(), CustomSerdes.Q2Output()));
+        dailyResults.toStream().to("q2-daily", Produced.with(Serdes.String(), CustomSerdes.Q2Output()));
+        weeklyResults.toStream().to("q2-weekly", Produced.with(Serdes.String(), CustomSerdes.Q2Output()));
 
         final KafkaStreams streams = new KafkaStreams(builder.build(), props);
 
-        Thread metricThread = new Thread(){
-            @Override
-            public void run() {
-                while(true) {
-                    streams.metrics().forEach((metricName, metric) -> {
-                        if (metricName.name().contentEquals("process-rate") && metricName.group().contentEquals("stream-thread-metrics"))
-                            System.out.println(metricName.name() + "  " + metricName.group() + "  = " + metric.metricValue());
-
-                        if (metricName.name().contentEquals("process-latency-avg") && metricName.group().contentEquals("stream-thread-metrics")) {
-                            System.out.println(metricName.name() + "  " + metricName.group() + "  = " + metric.metricValue());
-                        }
-
-                    });
-                    try {
-                        sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        };
         streams.cleanUp(); //clean up of the local StateStore
-        metricThread.start();
         streams.start();
 
 
         // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
-//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-//            streams.metadataForLocalThreads().forEach(threadMetadata -> threadMetadata.threadState());
-////            streams.metrics().forEach((metricName, metric) -> System.out.println(metricName + " -> Metric: "+metric.metricValue()));
-//            streams.close();
-//        }));
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
