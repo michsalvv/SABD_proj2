dfab5a8542ac8d4948bd33e32d3936c0736c220f
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2022-07-01 12:51:00 +0200

    Updated Scripts & README

--- scripts/run_query_kafka.sh
+++ scripts/run_query_kafka.sh
@@ -1,12 +1,29 @@
 #! /bin/bash
+if [ "$#" != "1" ]; then
+    echo "USAGE: sudo sh scripts/run_query_kafka.sh [QUERY]"
+    echo " > [QUERY]: Q1 / Q2"
+    exit 1
+fi
+
+if [ "$1" != "Q1" ] ; then
+  if [ "$1" != "Q2" ] ; then
+      echo " > [QUERY]: Q1 / Q2"
+      exit 1
+  fi
+fi
+
+
 echo "------------------------------------------------------------"
-echo "Cleaning Flink Environment"
+echo "Cleaning Kafka Environment"
 echo "------------------------------------------------------------"
 sudo sh scripts/setup_env.sh 2>/dev/null
 
 echo "------------------------------------------------------------"
 echo "Starting Kafka Producer in Background"
 echo "------------------------------------------------------------"
+echo "INFO: jdk path is: '/usr/java/jdk-11.0.15/bin/java'. Change this script with your jdk-11 path."
+
+# Change here java path
 /usr/java/jdk-11.0.15/bin/java -cp target/SABD_proj2-1.0.jar kafka.Producer 1> /dev/null 2>/dev/null & 
 sleep 0.4
 echo "..."
@@ -16,8 +33,8 @@ sleep 0.4
 echo "..."
 sleep 0.4
 echo "Producer Started"
-sleep 1
+sleep 0.5
 echo "------------------------------------------------------------"
-echo "Running Query $1 with parallelism $2"
+echo "Running Query $1"
 echo "------------------------------------------------------------"
-sudo docker exec -it jobmanager /bin/sh -c "flink run -c flink.Main /home/SABD_proj2-1.0.jar $1 $2"
+/usr/java/jdk-11.0.15/bin/java -cp target/SABD_proj2-1.0.jar kafka.Main $1
--- src/main/java/flink/queries/process/CellStatistics.java
+++ src/main/java/flink/queries/process/CellStatistics.java
@@ -4,7 +4,6 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.dropwizard.metrics.DropwizardMeterWrapper;
 import org.apache.flink.metrics.Meter;
 import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
-import org.apache.flink.streaming.api.functions.windowing.RichProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import utils.Tools;
